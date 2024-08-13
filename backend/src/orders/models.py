66bf11c9cc02989c0a6821f4a608538f797f6764
Tim PC <tim2004timi@mail.ru>
2024-08-14 02:11:09 +0300

    Add not full model for order

--- backend/src/orders/models.py
+++ backend/src/orders/models.py
@@ -1,10 +1,32 @@
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship, declared_attr
+from datetime import datetime
 
 from ..database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Order(Base):
+    __tablename__ = "orders"
 
-    username: Mapped[str] = mapped_column(index=True, nullable=False, unique=True)
-    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    full_name: Mapped[str] = mapped_column(nullable=False)
+    order_date: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.utcnow
+    )
+    status: Mapped[str] = mapped_column(
+        nullable=False, default="в обработке"
+    )  # TODO Указать дефолт
+    messages: Mapped[str] = mapped_column(nullable=True, default=None)
+    tag: Mapped[str] = mapped_column(nullable=True, default=None)
+    channel: Mapped[str] = mapped_column(nullable=True, default=None)
+    address: Mapped[str] = mapped_column(nullable=False)
+    task: Mapped[str] = mapped_column(nullable=True, default=None)
+    note: Mapped[str] = mapped_column(nullable=True, default=None)
+    comment: Mapped[str] = mapped_column(nullable=True, default=None)
+    storage: Mapped[str] = mapped_column(nullable=True, default=None)
+    project: Mapped[str] = mapped_column(nullable=True, default=None)
+    phone_number: Mapped[str] = mapped_column(nullable=True, default=None)
+    email: Mapped[str] = mapped_column(nullable=True, default=None)
+    summ: Mapped[int] = mapped_column(nullable=True, default=0)
+
+    # products
+    # files
