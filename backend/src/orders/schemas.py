881ca017a9018af8299909654598d876d860d82f
Tim PC <tim2004timi@mail.ru>
2024-08-14 02:26:58 +0300

    Add schemas to orders v1

--- backend/src/orders/schemas.py
+++ backend/src/orders/schemas.py
@@ -1,4 +1,43 @@
-from pydantic import BaseModel
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, EmailStr
 from datetime import datetime
 
 
+class OrderBase(BaseModel):
+    full_name: str
+    status: str | None = None
+    tag: str | None = None
+    channel: str | None = None
+    address: str
+    task: str | None = None
+    note: str | None = None
+    comment: str | None = None
+    storage: str | None = None
+    project: str | None = None
+    phone_number: str | None = None
+    email: EmailStr | None = None
+
+    # products
+    # files
+
+
+class OrderCreate(OrderBase):
+    pass
+
+
+class OrderUpdatePartial(OrderBase):
+    full_name: str | None = None
+    address: str | None = None
+
+
+class ProfileDelete(BaseModel):
+    id: int
+
+
+class Order(OrderBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    messages: str | None
+    summ: int
