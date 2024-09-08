767a65715c629413327f3c76b7d928f78683cf58
Tim PC <tim2004timi@mail.ru>
2024-09-08 11:58:34 +0300

    add users package

--- backend/src/users/schemas.py
+++ backend/src/users/schemas.py
@@ -3,16 +3,16 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
-class UserBase(BaseModel):
+class AdminBase(BaseModel):
     username: str
     email: EmailStr
 
 
-class UserCreate(UserBase):
+class AdminCreate(AdminBase):
     password: str
 
 
-class User(UserBase):
+class Admin(AdminBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
