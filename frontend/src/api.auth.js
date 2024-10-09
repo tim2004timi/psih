c0818331dfa30b3973ea9ad94f9e9edc79a3fcc5
Kruchinkin Sergey <kruchinkin.s@yandex.ru>
2024-10-10 00:17:44 +0300

    add new Content-Type for login

--- frontend/src/api.auth.js
+++ frontend/src/api.auth.js
@@ -1,8 +1,16 @@
 import { instance } from "./api.config.js";
 
 const AuthService = {
-    login(email, password) {
-        return instance.post("/api/jwt/login/", { email, password });
+    login(username, password) {
+        const data = new URLSearchParams();
+        data.append('username', username);
+        data.append('password', password);
+        
+        return instance.post("/api/jwt/login/", data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
     },
     
     refreshToken() {
