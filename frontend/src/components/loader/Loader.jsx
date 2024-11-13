4f2b65fb33cf3fd4af2ad147027f7167b53a6bf8
Kruchinkin <kruchinkin.s@yandex.ru>
2024-11-13 21:18:56 +0300

    add loader

--- frontend/src/components/loader/Loader.jsx
+++ frontend/src/components/loader/Loader.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Loader.css';
+import logo from '../../assets/img/logo.svg'
 
 const Loader = () => {
     return (
       <div class="loader-container">
         <div class="loader">
-          <div class="loader-bar"></div>
-          <div class="loader-bar"></div>
+          <div className="bar bar1"></div>
+          <div className="bar bar2"></div>
         </div>
+        <a className="logo">
+          <img src={logo} alt="logo" className="logo__img" />
+        </a>
       </div>
     );
 }
